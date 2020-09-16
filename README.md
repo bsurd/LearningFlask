92a39a0f850d6e4ca0f628bd15599afb6576f75d
bsurd <surd.bogdan@gmail.com>
2020-09-16 13:59:46 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# LearningFlask
+## LearningFlask :construction:
+- :information_source: Trying to get the hand of this following [the flask mega-tutorial](https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world) by [Miguel Grinberg](https://github.com/miguelgrinberg)
+
+#### Chapters
+1. :heavy_check_mark: Hello, World!
+2. :heavy_check_mark: Templates
+3. :eyes: Web Forms
+4. Database
+5. User Logins
+6. Profile Page and Avatars
+7. Error Handling
+8. Followers
+9. Pagination
+10. Email Support
+11. Facelift
+12. Dates and Times
+13. I18n and L10n
+14. Ajax
+15. A Better Application Structure
+16. Full-Text Search
+17. Deployment on Linux
+18. Deployment on Heroku
+19. Deployment on Docker Containers
+20. Some JavaScript Magic
+21. User Notifications
+22. Background Jobs
+23. Application Programming Interfaces (APIs)
---
+++ app/routes.py
@@ -0,0 +1,118 @@
+from flask import render_template, make_response, request
+from app import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Bogdan'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+# @app.route('/get')
+# def get_cookie():
+#     myapp = request.cookies.get("myapp")
+#     return "Cookie Content Is " + str(myapp)
+#
+#
+# @app.route('/set')
+# def set_cookie():
+#     response = make_response("I have set the cookie")
+#     response.set_cookie("my_app", "Flask Web Development")
